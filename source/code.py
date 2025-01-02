782dd8d36e0acf30782efed25257fad185b8d184
Chakri <chakri.r964@gmail.com>
2025-01-02 12:22:07 +0530

    added print format

--- source/code.py
+++ source/code.py
@@ -1,3 +1,5 @@
 from main_code import additoion,subtra
+value_addition =additoion(5,6,7)
+value_sub=subtra(5,9)
+print('value of addition:{0} and subtraction:{1}'.format(value_addition,value_sub))
 
-additoion(5,5,6)
